dd2898f14a69fb9f0444ca838338540ccdc1d991
Nishant Patel <nishantuwindsor@gmail.com>
2017-12-13 21:03:30 -0500

    Check email address present in database or not before creating a new account

--- app/src/main/java/com/example/android/udninventory/login/NewAccountActivity.java
+++ app/src/main/java/com/example/android/udninventory/login/NewAccountActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.udninventory.login;
 
+import android.app.LoaderManager;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -21,7 +24,8 @@ import android.widget.TextView;
 import com.example.android.udninventory.R;
 import com.example.android.udninventory.data.ItemContract.ItemEntry;
 
-public class NewAccountActivity extends AppCompatActivity {
+public class NewAccountActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /* TAG for log messages*/
     private static final String LOG_TAG = NewAccountActivity.class.getName();
@@ -37,6 +41,12 @@ public class NewAccountActivity extends AppCompatActivity {
     private EditText mUserPassword;
 
     private Button mCreateAccountButton;
+
+    /* Flag to check email address provided by user exists in the database or not */
+    private boolean mEmailExistsFlag = false;
+
+    /* Loader id for creating a new account */
+    private static final int CREATE_NEW_ACCOUNT_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,42 +108,16 @@ public class NewAccountActivity extends AppCompatActivity {
             return;
         }
 
-        mCreateAccountButton.setEnabled(false);
+        // If flag value is false, that means email provided by user doesn't exists in database
+        // so initialize the loader to create a new account
+        if (!mEmailExistsFlag) {
+            getLoaderManager().initLoader(CREATE_NEW_ACCOUNT_LOADER_ID, null, this);
+        } else {
+            // If flag is true, that means email provided by user already exists in database
+            // so restart the loader and allow user to enter new email address for account
+            getLoaderManager().restartLoader(CREATE_NEW_ACCOUNT_LOADER_ID, null, this);
+        }
 
-        final ProgressDialog progressDialog = new ProgressDialog(
-                NewAccountActivity.this,
-                R.style.LoginTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = mUserName.getText().toString().trim();
-        String email = mUserEmail.getText().toString().trim();
-        String password = mUserPassword.getText().toString().trim();
-
-        final ContentValues contentValues = new ContentValues();
-        contentValues.put(ItemEntry.CREDENTIALS_TABLE_COLUMN_USER_NAME, name);
-        contentValues.put(ItemEntry.CREDENTIALS_TABLE_COLUMN_EMAIL, email);
-        contentValues.put(ItemEntry.CREDENTIALS_TABLE_COLUMN_PASSWORD, password);
-
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                // Insert data into Credentials database in background thread
-                Uri uri = getContentResolver().insert(ItemEntry.CREDENTIALS_CONTENT_URI, contentValues);
-                try {
-                    Thread.sleep(1500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                progressDialog.dismiss();
-                // If inserting data was successful, finish the activity
-                if (uri != null) {
-                    finish();
-                }
-            }
-        });
-        mCreateAccountButton.setEnabled(true);
     }
 
     /**
@@ -200,5 +184,96 @@ public class NewAccountActivity extends AppCompatActivity {
             Log.i(LOG_TAG, "\nid:" + idCheck + " Name:" + nameCheck + " Email:" + emailCheck + " Password:" + passwordCheck);
         }
         cursor.close();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Since we have to make sure that user can not enter the same email address twice
+        // while creating a new account, so set the projection for email address column
+        String[] projection = {
+                ItemEntry._ID,
+                ItemEntry.CREDENTIALS_TABLE_COLUMN_EMAIL};
+
+        // where clause for email address column, because we want to check if the email provided by
+        // user is present in the email column or not
+        String selection = ItemEntry.CREDENTIALS_TABLE_COLUMN_EMAIL + "=?";
+
+        // Email provided by user when creating a new account
+        String email = mUserEmail.getText().toString().trim();
+        String[] selectionArgs = new String[]{email};
+
+        return new CursorLoader(
+                this,                       // Context of the app
+                ItemEntry.CREDENTIALS_CONTENT_URI,  // Uri to query Credentials table
+                projection,                         // Projection of which column of table we are interested in
+                selection,                          // Where clause
+                selectionArgs,                       // Selection args
+                null);                     // Sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // If email address is present in the table, set the error message about email address
+        // already exists in database
+        if (cursor.moveToFirst()) {
+            mUserEmailWrapper.setError("Email address already exists");
+            mEmailExistsFlag = true;
+        } else {
+            mUserEmailWrapper.setErrorEnabled(false);
+            insertUserDetails();
+            mEmailExistsFlag = false;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
+    /**
+     * This method is called to create a new account by
+     * inserting details provided by used into database
+     */
+    private void insertUserDetails() {
+        // Progress dialog for better user experience
+        final ProgressDialog progressDialog = new ProgressDialog(
+                NewAccountActivity.this,
+                R.style.LoginTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+        // Prevent Progress dialog from dismissing when user click on rest of the screen
+        progressDialog.setCancelable(false);
+
+        // Get the text provided by user
+        String name = mUserName.getText().toString().trim();
+        String email = mUserEmail.getText().toString().trim();
+        String password = mUserPassword.getText().toString().trim();
+
+        // Content value object to insert data entered by user into database
+        final ContentValues contentValues = new ContentValues();
+        contentValues.put(ItemEntry.CREDENTIALS_TABLE_COLUMN_USER_NAME, name);
+        contentValues.put(ItemEntry.CREDENTIALS_TABLE_COLUMN_EMAIL, email);
+        contentValues.put(ItemEntry.CREDENTIALS_TABLE_COLUMN_PASSWORD, password);
+
+        // Run AsyncTask to add data into database in background thread,
+        // without disturbing UI thread
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Insert data into Credentials database
+                Uri uri = getContentResolver().insert(ItemEntry.CREDENTIALS_CONTENT_URI, contentValues);
+                try {
+                    // Put thread to sleep for 2seconds to show user a progress dialog
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                progressDialog.dismiss();
+                // If inserting data was successful, finish the activity
+                if (uri != null) {
+                    finish();
+                }
+            }
+        });
     }
 }
